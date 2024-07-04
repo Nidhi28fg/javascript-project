5f3c9fd9c02055a740e08c7b892d203f0e12b9f0
Nidhi Singh Bhadoria <165938813+Nidhi28fg@users.noreply.github.com>
2024-07-04 17:32:31 +0000

    variable

--- 01-basic/test.js
+++ 01-basic/test.js
@@ -1,5 +1,39 @@
 console.log("nidhi")
 
 const a = 10;
+/**
+These are constant values and cannot be changed.
+ */
 let b = 20;
+b = 30;
+
+// These are let values and can be changed. 
+// In JavaScript, the equal sign (=) is an "assignment" operator, not an "equal to" operator
+
 console.log( a + b);
+
+// Strings are written inside double or single quotes. Numbers are written without quotes.
+
+let carName;
+console.log(carName);
+// After the declaration, the variable has no value (technically it is undefined).
+
+
+var carName1 = "Volvo";
+var carName1;
+console.log(carName1);
+
+
+let x = "5" + 2 + 3;
+console.log(x);
+
+// if you add string first then call number will be declare string
+
+let y = 2 + 3 + "5";
+console.log(y);
+// if you add number first then all value will be declare numbers
+
+
+
+
+
