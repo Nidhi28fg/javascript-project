67fa57e775a2149bc50d540acd1e61adbb0a6f75
Nidhi Singh Bhadoria <165938813+Nidhi28fg@users.noreply.github.com>
2024-07-05 09:20:26 +0000

    let and const

---
+++ 01-basic/day2.js
@@ -0,0 +1,25 @@
+"use strict" /* treat all code as newer version */
+
+// You can create a constant array:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// You can change an element:
+cars[0] = "Toyota";
+
+// You can add an element:
+cars.push("Audi");
+
+console.log(cars);
+
+
+// You can create a const object:
+const car = {type:"Fiat", model:"500", color:"white"};
+
+// You can change a property:
+car.color = "red";
+
+// You can add a property:
+car.owner = "Johnson";
+
+
+console.log(car);
