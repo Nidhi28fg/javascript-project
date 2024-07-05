05182ff686f74a71f1d82874e2b549595285e97f
Nidhi Singh Bhadoria <165938813+Nidhi28fg@users.noreply.github.com>
2024-07-05 07:13:01 +0000

    where can we acess x value

--- 01-basic/test.js
+++ 01-basic/test.js
@@ -1,39 +1,66 @@
-console.log("nidhi")
+// console.log("nidhi")
 
-const a = 10;
-/**
-These are constant values and cannot be changed.
- */
-let b = 20;
-b = 30;
+// const a = 10;
+// /**
+// These are constant values and cannot be changed.
+//  */
+// let b = 20;
+// b = 30;
 
-// These are let values and can be changed. 
-// In JavaScript, the equal sign (=) is an "assignment" operator, not an "equal to" operator
+// // These are let values and can be changed. 
+// // In JavaScript, the equal sign (=) is an "assignment" operator, not an "equal to" operator
 
-console.log( a + b);
+// console.log( a + b);
 
-// Strings are written inside double or single quotes. Numbers are written without quotes.
+// // Strings are written inside double or single quotes. Numbers are written without quotes.
 
-let carName;
-console.log(carName);
-// After the declaration, the variable has no value (technically it is undefined).
-
-
-var carName1 = "Volvo";
-var carName1;
-console.log(carName1);
+// let carName;
+// console.log(carName);
+// // After the declaration, the variable has no value (technically it is undefined).
 
 
-let x = "5" + 2 + 3;
-console.log(x);
+// var carName1 = "Volvo";
+// var carName1;
+// console.log(carName1);
 
-// if you add string first then call number will be declare string
 
-let y = 2 + 3 + "5";
+// let x = "5" + 2 + 3;
+// console.log(x);
+
+// // if you add string first then all value will be declare string
+
+// let y = 2 + 3 + "5";
+// console.log(y);
+// // if you add number first then all value will be declare numbers
+
+// // block code
+// let  x = 10;
+// // Here x is 10
+
+// {  
+//   let x = 2;
+//   // Here x is 2
+// }
+
+// // Here x is 10
+
+// // There is no value of x in block code.. console take the value of globle let.
+// let  x = 10;
+// // Here x is 10
+
+// {  
+//   console.log(x);
+//   // Here x is 2
+// }
+
+// // Here x is 10
+
+
+// if There is no value in globle let. then 2nd console.log never excess block code x's value.
+{  
+  let y = 2;
+  console.log(y);
+  // Here x is 2
+}
 console.log(y);
-// if you add number first then all value will be declare numbers
-
-
-
-
-
+// Here x is 10
