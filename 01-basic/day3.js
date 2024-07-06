7e2f465dd7080cdd3280d48b95fdc2c91c563ea3
Nidhi Singh Bhadoria <165938813+Nidhi28fg@users.noreply.github.com>
2024-07-06 17:16:14 +0000

    splice method

--- 01-basic/day3.js
+++ 01-basic/day3.js
@@ -2,3 +2,7 @@ let fruits = ["Apple", "Banana"];
 fruits.push("Cherry");
 console.log(fruits); // ["Apple", "Banana", "Cherry"]
 
+let fruit = ["Apple", "Banana", "Cherry"];
+// Remove 1 element at index 1, and insert "Date" and "Elderberry"
+fruit.splice(0, 2, "Date", "Elderberry", "jhdjhfjs");
+console.log(fruit); // ["Apple", "Date", "Elderberry", "Cherry"]
