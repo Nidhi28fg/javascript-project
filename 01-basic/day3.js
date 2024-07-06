a8c52f92f4ced897fe42f6493adb6cdf6e601c40
Nidhi Singh Bhadoria <165938813+Nidhi28fg@users.noreply.github.com>
2024-07-06 17:35:47 +0000

    slice

--- 01-basic/day3.js
+++ 01-basic/day3.js
@@ -6,3 +6,7 @@ let fruit = ["Apple", "Banana", "Cherry"];
 // Remove 1 element at index 1, and insert "Date" and "Elderberry"
 fruit.splice(0, 2, "Date", "Elderberry", "jhdjhfjs");
 console.log(fruit); // ["Apple", "Date", "Elderberry", "Cherry"]
+
+let greeting = "Hello, World!";
+let world = greeting.slice(7, 13);
+console.log(world); // "World"
