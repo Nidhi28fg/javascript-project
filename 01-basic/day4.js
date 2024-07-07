82a6cfba02c887efeb65aaa67802302869f045c9
Nidhi Singh Bhadoria <165938813+Nidhi28fg@users.noreply.github.com>
2024-07-07 03:00:31 +0000

    termary operation

--- 01-basic/day4.js
+++ 01-basic/day4.js
@@ -13,10 +13,17 @@ function Person(name, age) {
   this.name = name;
   this.age = age;
 }
-let person = new Person('John', 28);
-let son = new Person('nidhi', 32)
-console.log(son)
-console.log(person)
+let John = new Person('John', 28);
+let Nidhi = new Person('nidhi', 32)
+console.log(John)
+console.log(Nidhi)
 
 // void operator
 let result = void 0; // result is undefined
+
+Ternary Operator
+
+let a = 27;
+let b =76;
+let results = (a > b) ? "a is greater" : "a is not greater";
+console.log(results); // "a is greater"
