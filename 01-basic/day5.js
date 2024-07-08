f820f0878ec59c0f179a18c4db8bc53857e093ac
Nidhi Singh Bhadoria <165938813+Nidhi28fg@users.noreply.github.com>
2024-07-08 13:35:58 +0000

    object2

--- 01-basic/day5.js
+++ 01-basic/day5.js
@@ -17,14 +17,24 @@ const datea = new Date();
 console.log(datea.toLocaleDateString());
 
 
+
+
+
+
+
+
+
+// interview question
+const myArr = [0, 1, 2, 3, 4, 5] 
 console.log("A ", myArr);
 
-const myn1 = myArr.slice(1, 3)
-
+const myn1 = myArr.slice(1, 3) /* count start from index 1 and end on 2 index*/
+console.log("a.", myArr);
 console.log(myn1);
-console.log("B ", myArr);
+console.log("B ", myArr);/* origanal data m change nhi hua hai*/
 
 
-const myn2 = myArr.splice(1, 3)
-console.log("C ", myArr);
+const myn2 = myArr.splice(1, 3) /* COUNT START FROM INDEX 1 AND END ON 3 INDEX */
+console.log("C ", myArr); /* here changed in orginal data,  */
 console.log(myn2);
+console.log("d ", myArr); /* here changed in orginal data , orignal array manupulate ho jata hai */
