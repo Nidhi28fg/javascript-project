e9558db6152f17cbe0067cb4e839b7a9fd2ba483
Nidhi Singh Bhadoria <165938813+Nidhi28fg@users.noreply.github.com>
2024-07-08 06:47:59 +0000

    mathfloor

---
+++ 01-basic/day5.js
@@ -0,0 +1,13 @@
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+console.log(Math.floor(3.9)) 
+
+// how to get two digit number
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+console.log(Math.floor(Math.random()))
+console.log(Math.floor(Math.random() * (max - min + 1)))
+console.log(Math.random() * (max - min + 1))
